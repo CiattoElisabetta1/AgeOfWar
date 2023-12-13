0e95f7f652dd417938ecd6cb553d2bc069524b94
UTENTE 1 <UTENTE 1@PC1-LAB3>
2023-12-13 09:55:29 +0100

    Aggiungere i file di progetto.

---
+++ AgeOfWar/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AgeOfWar.Personaggi;
+
+Console.WriteLine("Hello, World!");
+
+
+Cavaliere CavalirereRosso = new Cavaliere(1000);
+
+Cavaliere[] squadraRossa = new Cavaliere[100];
+
+for (int i = 0; i < 100; i++)
+{
+    squadraRossa[i] = new Cavaliere(1000);
+    squadraRossa[i].Squadra = "Squadra Rossa";
+}
